94a0aee97d3b78082b58ab86e85ce1e9857259bd
valeriaCifarattiReply <v.cifaratti@reply.it>
2025-03-29 08:37:42 +0100

    add icon for nav bottom bar

--- app/src/main/java/com/poliba/pwdmanagerpolibaproject/navigation/BottomNavigationItem.kt
+++ app/src/main/java/com/poliba/pwdmanagerpolibaproject/navigation/BottomNavigationItem.kt
@@ -3,9 +3,9 @@ package com.poliba.pwdmanagerpolibaproject.navigation
 import com.poliba.pwdmanagerpolibaproject.R
 
 sealed class BottomNavigationItem(val route: String, val icon: Int, val title: String) {
-    object Home : BottomNavigationItem("HOME", R.drawable.ic_launcher_foreground, "Home")
-    object GeneratePassword : BottomNavigationItem("GENERATE_PASSWORD", R.drawable.ic_launcher_foreground, "Generate")
-    object Profile : BottomNavigationItem("PROFILE", R.drawable.ic_launcher_foreground, "Profile")
+    object Home : BottomNavigationItem("HOME", R.drawable.icon_home, "Home")
+    object GeneratePassword : BottomNavigationItem("GENERATE_PASSWORD", R.drawable.icon_new_pwd, "Generate")
+    object Profile : BottomNavigationItem("PROFILE", R.drawable.icon_user, "Profile")
 
     companion object {
         fun getAll() = listOf(Home, GeneratePassword, Profile)
