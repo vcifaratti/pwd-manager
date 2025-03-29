efb4e2d21a0e1a30c0abb99fd620251913b4e3c7
valeriaCifarattiReply <v.cifaratti@reply.it>
2025-03-29 10:28:07 +0100

    add new test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,4 +116,9 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.auth)
     implementation(libs.firebase.database)
+
+    // Testing
+    androidTestImplementation(platform("androidx.compose:compose-bom:2024.02.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 }
